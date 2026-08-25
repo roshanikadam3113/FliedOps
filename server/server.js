1319e4e56a6656d545433931fea4c4d5cb10a635
roshnaikadam3113 <roshanikadam051@gmail.com>
2026-08-25 23:15:43 +0530

    fix: Update Express catch-all wildcard route to *all for path-to-regexp v8 compatibility

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use('/api/jobs', jobRoutes);
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
+  app.get('*all', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 } else {
